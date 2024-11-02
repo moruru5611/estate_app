2faebf280f11d579970fbc7c19f77000af66e601
moruru5611 <mm02060116@gmail.com>
2024-11-02 13:51:53 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from src.send_api import connect_api
+
+# secrets.tomlの取得
+api_key=st.secrets["api_key"]
+pref_id=st.secrets["pref_id"]
+change_index=st.secrets["change_index"]
+
+# フッターのHTMLとCSS
+footer = """
+    <style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+        color: black;
+        text-align: center;
+        padding: 10px;
+    }
+    </style>
+    <div class="footer">
+        <p>不動産価格（取引価格・成約価格）情報取得API（国土交通省）を基に、作成しています。</p>
+    </div>
+    """
+
+# フッターを挿入
+st.markdown(footer, unsafe_allow_html=True)
+
+st.title("不動産取引情報検索")
+
+input_col1, input_col2, input_col3 = st.columns(3)
+with input_col1:
+    prefecture = st.selectbox(
+        label="都道府県", options=[index for index, i in pref_id.items()]
+    )
+with input_col2:
+    city = st.text_input(label="市町村（空白可）", placeholder="大阪市")
+with input_col3:
+    year = st.number_input(label="取引時期（西暦）", value=2024, step=0)
+
+search = connect_api(api_key,prefecture, city, year) if st.button("検索") else None
---
+++ src/send_api.py
@@ -0,0 +1,69 @@
+# APIによるデータ取得
+
+# インポート
+import streamlit as st
+import requests
+import pandas as pd
+
+# apiキーの取得
+api_key = st.secrets["api_key"]
+my_url = st.secrets["my_url"]
+pref_id = st.secrets["pref_id"]
+change_index = st.secrets["change_index"]
+
+
+# API接続関数
+def connect_api(api_key: str | int, prefecture: str, city: str | None, year: int):
+    api_key = api_key
+    url = "https://www.reinfolib.mlit.go.jp/ex-api/external/XIT002"
+    headers = {"Ocp-Apim-Subscription-Key": f"{api_key}"}
+    params = {"year": year, "area": pref_id[prefecture]}
+    response = requests.get(url, headers=headers, params=params)
+    json_data = response.json()
+
+    #  市町村コードの取得
+    def send_city_id(json_data, city: str | None):
+        try:
+            for item in json_data["data"]:
+                if item["name"] == city:
+                    return item["id"]
+            if city == "":
+                return None
+            else:
+                return "該当する市町村が存在しません"
+        except KeyError:
+            return "該当する市町村が存在しません"
+
+    #  入力された市町村が存在しない場合
+    city_id = send_city_id(json_data, city)
+    url = "https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001"
+    params["city"] = city_id
+    response = requests.get(url, headers=headers, params=params)
+    json_data = response.json()
+
+    #  検索結果に応じた内容を画面描画
+    try:
+        df = pd.DataFrame(json_data["data"])
+        df["TradePrice"] = df["TradePrice"].apply(lambda x: f"¥{int(x):,}")
+        df = df.drop(
+            columns=[
+                "PriceCategory",
+                "Prefecture",
+                "MunicipalityCode",
+                "UnitPrice",
+                "Frontage",
+                "PricePerUnit",
+            ]
+        )
+        df = df.rename(columns=change_index)
+        df.rename(
+            columns={"用途地区": "取引価格", "取引価格": "用途地区"}, inplace=True
+        )
+        df["取引価格"], df["用途地区"] = df["用途地区"], df["取引価格"]
+        st.link_button("空き家を登録してみる（外部サイト）", url=my_url)
+        st.write(f"{year}年の{prefecture}{city}の取引情報")
+        st.write(f"取引件数{str(len(df))}件")
+        st.dataframe(df)
+
+    except KeyError:
+        st.write("該当する市町村が存在しません")
